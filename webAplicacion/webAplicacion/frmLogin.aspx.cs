d97a63a153213e69a79f477f0d68a39e98144598
d4vyncy <davidserrudo@gmail.com>
2024-01-24 17:15:22 -0400

    ejemplo consumo de servicio

---
+++ webAplicacion/webAplicacion/frmLogin.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using webAplicacion.Servicios;
+using webAplicacion.Servicios.Models;
+
+namespace webAplicacion
+{
+    public partial class frmLogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            servicioAutenticar oservicioAutenticar = new servicioAutenticar();
+            usuarioLogin ousuarioLogin = oservicioAutenticar.RecuperaToken("dav", "12345");
+        }
+    }
+}
